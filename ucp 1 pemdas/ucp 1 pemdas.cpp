a71a3333d44bfce1f978fb8e8b9c4de80334830f
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-05-05 18:10:54 +0700

    Add project files.

---
+++ ucp 1 pemdas/ucp 1 pemdas.cpp
@@ -0,0 +1,38 @@
+// ucp 1 pemdas.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+
+	matematika = rand() % 10;
+	bahasa inggris  = rand() % 10;
+
+
+	if (bilangan1 == bilangan2)
+	{+
+		
+		= "bilangan satu samadengan bilangan dua";
+	}
+	else if (bilangan1 > bilangan2)
+	{
+		
+		= "bilangan satu lebih besar dari bilangan dua";
+	}
+	else
+	{
+		
+		= "bilangan satu lebih kecil dari bilangan dua";
+
+	}
+	cout << "bilangan pertama : " << bilangan1 << endl;
+	cout << "bilangan kedua : " << bilangan2 << endl;
+	cout << "status bilangan : " << status;
+
+	// 
+	//
+	// contoh implementasi dari stuct deperti nim,nama,kelas
